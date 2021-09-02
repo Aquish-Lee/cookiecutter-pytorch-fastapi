74d9a127f6ace369cf1645e06aa4ee04641540aa
Aquish-Lee <51768704+Aquish-Lee@users.noreply.github.com>
2021-09-03 00:13:28 +0800

    Update release.sh

--- {{cookiecutter.project_slug}}/release.sh
+++ {{cookiecutter.project_slug}}/release.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 # 构建 Docker镜像并发布至镜像仓库
-docker build -t {{cookiecutter.project_name}}:latest .
+docker build -t {{cookiecutter.project_slug}}:latest .
 echo "docker build success..."
 docker login --username=aqu****** registry.cn-hangzhou.aliyuncs.com  # 修改为自己的仓库源
 echo "docker login success"
-docker tag {{cookiecutter.project_name}}:latest registry.cn-hangzhou.aliyuncs.com/idcard/idcard_project:{{cookiecutter.project_name}}_latest
+docker tag {{cookiecutter.project_slug}}:latest registry.cn-hangzhou.aliyuncs.com/idcard/idcard_project:{{cookiecutter.project_slug}}_latest
 echo "docker tag success"
-docker push registry.cn-hangzhou.aliyuncs.com/idcard/idcard_project:{{cookiecutter.project_name}}_latest
+docker push registry.cn-hangzhou.aliyuncs.com/idcard/idcard_project:{{cookiecutter.project_slug}}_latest
 echo "docker push success"
